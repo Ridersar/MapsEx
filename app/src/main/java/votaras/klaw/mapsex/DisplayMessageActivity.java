63982f5edeb6ec843fe50c7cbfb604968f452e3a
Gnosheva <alena_gnosheva@mail.ru>
2020-04-17 00:24:04 +0400

    Button "Создать Маршрут"

---
+++ app/src/main/java/votaras/klaw/mapsex/DisplayMessageActivity.java
@@ -0,0 +1,29 @@
+package votaras.klaw.mapsex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Получаем сообщение из объекта intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Создаем текстовое поле
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Устанавливаем текстовое поле в системе компоновки activity
+        setContentView(textView);
+    }
+}
--- app/src/main/java/votaras/klaw/mapsex/MainActivity.java
+++ app/src/main/java/votaras/klaw/mapsex/MainActivity.java
@@ -16,6 +16,11 @@ import com.yandex.mapkit.mapview.MapView;
 
 import java.util.ArrayList;
 
+import android.content.Intent;//подключаем класс Intent
+import android.os.Bundle;
+import android.view.View; // подключаем класс View для обработки нажатия кнопки
+import android.widget.EditText; // подключаем класс EditText
+
 public class MainActivity extends Activity {
     /**
      * Замените "your_api_key" валидным API-ключом.
@@ -120,6 +125,21 @@ public class MainActivity extends Activity {
 
     }
 
-
+    public final static String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+    // Метод обработки нажатия на кнопку
+    public void sendMessage(View view) {
+        // действия, совершаемые после нажатия на кнопку
+        // Создаем объект Intent для вызова новой Activity
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // Получаем текстовое поле в текущей Activity
+        // EditText editText = (EditText) findViewById(R.id.edit_message);
+        // Получае текст данного текстового поля
+        // String message = editText.getText().toString();
+        // Добавляем с помощью свойства putExtra объект - первый параметр - ключ,
+        // второй параметр - значение этого объекта
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        // запуск activity
+        startActivity(intent);
+    }
 
 }
