3d112fc2a501fa06272e17272caab3e74de27293
OlFil <miss-olenka2000@yandex.ru>
2020-04-30 20:29:58 +0400

    Нерабочее, не пойму в чем дело
    Здесь попытка геокодирования через лиьу андроида и попытка отрисовки маршрута. Но чего-то не хватает, никак не пойму чего.

--- app/src/main/java/votaras/klaw/mapsex/GoRoute.java
+++ app/src/main/java/votaras/klaw/mapsex/GoRoute.java
@@ -2,25 +2,41 @@ package votaras.klaw.mapsex;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.location.Address;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.RequestPoint;
+import com.yandex.mapkit.RequestPointType;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.geometry.Polyline;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.map.MapObjectCollection;
 import com.yandex.mapkit.map.PolylineMapObject;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.transport.Transport;
+import com.yandex.mapkit.transport.TransportFactory;
+import com.yandex.mapkit.transport.masstransit.PedestrianRouter;
+import com.yandex.mapkit.transport.masstransit.Route;
+import com.yandex.mapkit.transport.masstransit.Session;
+import com.yandex.mapkit.transport.masstransit.TimeOptions;
+import com.yandex.runtime.Error;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Intent; //подключаем класс Intent
 import android.os.Bundle;
 import android.view.View; // подключаем класс View для обработки нажатия кнопки
 import android.widget.Button;
 import android.widget.EditText; // подключаем класс EditText
+import android.location.Geocoder;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 public class GoRoute extends Activity {
     /**
@@ -33,6 +49,7 @@ public class GoRoute extends Activity {
     private final Point point2 = new Point(51.527569, 46.011533);
     private final Point point3 = new Point(51.525753, 46.010434);
     private final Point point4 = new Point(51.520771, 46.002646);
+   private Point p1,p2;
 
 
     private final Point MagentaRoute1 = new Point(51.528021,46.021923);
@@ -47,6 +64,55 @@ public class GoRoute extends Activity {
     private MapObjectCollection mapObjects;
     private Handler animationHandler;
     private Button btn_create;
+    //попытка геокода
+  /*  public Point getLocationFromAddress(String strAddress) throws IOException {
+
+      Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        Point p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress,5);
+            if (address==null) {
+                return null;
+            }
+            Address location=address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new Point((double) (location.getLatitude() * 1E6),
+                    (double) (location.getLongitude() * 1E6));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return p1;
+    }*/
+    public void routeBetween(double longitude, double latitude, double lat, double lon){
+        Transport transport = TransportFactory.getInstance();
+        PedestrianRouter router = transport.createPedestrianRouter();
+        ArrayList<RequestPoint> requests = new ArrayList<>();
+        requests.add(new RequestPoint(new Point(latitude, longitude), RequestPointType.WAYPOINT, ""));
+        requests.add(new RequestPoint(new Point(lat, lon), RequestPointType.WAYPOINT, ""));
+        router.requestRoutes(requests, new TimeOptions(), new Session.RouteListener() {
+            @Override
+            public void onMasstransitRoutes(@NonNull List<com.yandex.mapkit.transport.masstransit.Route> list) {
+                System.out.println(list.toString());
+                if(!list.isEmpty()){
+                    mapView.getMap().getMapObjects().clear();
+                }
+                for(Route route: list){
+                    mapView.getMap().getMapObjects().addPolyline(list.get(0).getGeometry());
+                }
+            }
+
+            @Override
+            public void onMasstransitRoutesError(@NonNull Error error) {
+                System.out.println(error);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,18 +148,53 @@ public class GoRoute extends Activity {
         String A = intent.getStringExtra(ActivityDisplayMessage.A_str);
         String B = intent.getStringExtra(ActivityDisplayMessage.B_str);
 
+
+        ArrayList<RequestPoint> requests = new ArrayList<>();
+        //Geocoder geocoder = new Geocoder(getApplicationContext());
+        //List<Address> addresses;
+        //ArrayList<Point> polylinePoints = new ArrayList<>(); //создание списка точек
+
+//попытка геокодирования
+     /*   try {
+            addresses = geocoder.getFromLocationName(A, 1);
+            addresses=geocoder.getFromLocationName(B,1);
+            if(addresses.size() > 0) {
+                double latitude= addresses.get(0).getLatitude();
+                double longitude= addresses.get(0).getLongitude();
+                double latitude1= addresses.get(1).getLatitude();
+                double longitude1= addresses.get(1).getLongitude();
+               // polylinePoints.add(new Point(latitude, longitude)); //добавление точки в список
+                //polylinePoints.add(new Point(latitude1,longitude1)); //добавление точки в список
+                p1=new Point(latitude, longitude);
+                p2=new Point(latitude1, longitude1);
+            }
+
+        } catch (IOException e) {
+           // Toast.makeText(GoRoute.this,"Sorry, can you select again?", Toast.LENGTH_SHORT);
+            e.printStackTrace();
+        }*/
+        //PolylineMapObject polyline = mapObjects.addPolyline(new Polyline(polylinePoints)); //отрисовка маршрута по точкам списка
+        //polyline.setStrokeColor(Color.RED);
+
         //конвертирование в int
-        int a = Integer.parseInt(A); //начало
-        int b = Integer.parseInt(B); //конец
+        double aa=Double.parseDouble(A);
+        double bb=Double.parseDouble(B);
+        //int a = Integer.parseInt(A); //начало
+        //int b = Integer.parseInt(B); //конец
+        mapView.getMap().getMapObjects().addPlacemark(new Point(aa, bb));
+        try{
+            routeBetween(aa,bb,51.517547, 46.010487);
+        }catch (NullPointerException e){
+            System.out.println("GPS is off");
+        }
+        //ArrayList<OpPoint> mas = new ArrayList<OpPoint>(); //список опорных точек
+        //Route.createPoints(mas); //создание объектов
+        //Route.drawRoute(mas); //отрисовка маршрутов
 
-        ArrayList<OpPoint> mas = new ArrayList<OpPoint>(); //список опорных точек
-        Route.createPoints(mas); //создание объектов
-        Route.drawRoute(mas); //отрисовка маршрутов
+       // ArrayList<Point> route = Route.searchRoute(mas, a, b); //маршрут
 
-        ArrayList<Point> route = Route.searchRoute(mas, a, b); //маршрут
-
-        PolylineMapObject polylineRoute = mapObjects.addPolyline(new Polyline(route)); //отрисовка маршрута по точкам списка
-        polylineRoute.setStrokeColor(Color.RED);
+        //PolylineMapObject polylineRoute = mapObjects.addPolyline(new Polyline(route)); //отрисовка маршрута по точкам списка
+        //polylineRoute.setStrokeColor(Color.RED);
          //createMapObjects();
     }
 
@@ -116,19 +217,21 @@ public class GoRoute extends Activity {
     //создание объектов на карте
     private void createMapObjects() {
         ArrayList<Point> polylinePoints = new ArrayList<>(); //создание списка точек
-        polylinePoints.add(new Point(point1.getLatitude(), point1.getLongitude())); //добавление точки в список
+        polylinePoints.add(p1);
+        polylinePoints.add(p2);
+      /*  polylinePoints.add(new Point(point1.getLatitude(), point1.getLongitude())); //добавление точки в список
         polylinePoints.add(new Point(point2.getLatitude(), point2.getLongitude())); //добавление точки в список
         polylinePoints.add(new Point(point3.getLatitude(), point3.getLongitude())); //добавление точки в список
         polylinePoints.add(new Point(point4.getLatitude(), point4.getLongitude())); //добавление точки в список
         ArrayList<Point> polylinePointsAlena = new ArrayList<>(); //создание списка точек
         polylinePointsAlena.add(new Point(pointBlue1.getLatitude(), pointBlue1.getLongitude())); //добавление точки в список
         polylinePointsAlena.add(new Point(pointBlue2.getLatitude(), pointBlue2.getLongitude())); //добавление точки в список
-
+*/
 
         PolylineMapObject polyline = mapObjects.addPolyline(new Polyline(polylinePoints)); //отрисовка маршрута по точкам списка
         polyline.setStrokeColor(Color.RED);
-        PolylineMapObject polylineAlena = mapObjects.addPolyline(new Polyline(polylinePointsAlena)); //отрисовка маршрута по точкам списка
-        polylineAlena.setStrokeColor(Color.BLUE);
+  //      PolylineMapObject polylineAlena = mapObjects.addPolyline(new Polyline(polylinePointsAlena)); //отрисовка маршрута по точкам списка
+    //    polylineAlena.setStrokeColor(Color.BLUE);
         //polyline.setZIndex(100.0f);
 
         ArrayList<Point> polylinePointsMagenta = new ArrayList<>();
--- app/src/main/java/votaras/klaw/mapsex/MainActivity.java
+++ app/src/main/java/votaras/klaw/mapsex/MainActivity.java
@@ -18,6 +18,7 @@ import com.yandex.mapkit.mapview.MapView;
 
 
 
+
 import android.content.Intent; //подключаем класс Intent
 import android.os.Bundle;
 import android.view.View; // подключаем класс View для обработки нажатия кнопки
